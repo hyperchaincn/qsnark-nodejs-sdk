29309abf2d48ac308fb9cc7eecac36c3788b8fc1
yixiangguo <793123055@qq.com>
2018-05-22 18:47:54 +0800

    .

--- qsnart/conf.js
+++ qsnart/conf.js
@@ -1,4 +1,4 @@
-exports.ACCESS_KEY = 'c4bc5803-8f2c-43e6-9961-9ff3cbd1f733';
-exports.SECRET_KEY = '9f5qzgZval3O7YKqp44m4jM74m03IN9S';
-exports.PHONE = '18702604793';
-exports.PASSWORD = 'me123456';
+exports.ACCESS_KEY = '';
+exports.SECRET_KEY = '';
+exports.PHONE = '';
+exports.PASSWORD = '';
