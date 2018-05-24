e225f95e73ec6f0100adc9ae3a8a99479d70fbc8
yixiangguo <793123055@qq.com>
2018-05-24 15:56:04 +0800

    .

--- examples/index.js
+++ examples/index.js
@@ -1,9 +1,9 @@
 var qsnart = require('../index.js')
 
-qsnart.conf.ACCESS_KEY = 'c4bc5803-8f2c-43e6-9961-9ff3cbd1f733';
-qsnart.conf.SECRET_KEY = '9f5qzgZval3O7YKqp44m4jM74m03IN9S';
-qsnart.conf.PHONE = '18702604793';
-qsnart.conf.PASSWORD = 'me123456';
+qsnart.conf.ACCESS_KEY = '';
+qsnart.conf.SECRET_KEY = '';
+qsnart.conf.PHONE = '';
+qsnart.conf.PASSWORD = '';
 
 var api = new qsnart.core.Qsnack()
 
